0a478a1e7f94ce8b8f2393e67c9c6da662d50a13
Serhii Buria <sergeiburya@gmail.com>
2025-09-23 18:57:46 +0300

    initial project

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+group = "sb.ua"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/") {
+        name = "papermc-repo"
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks {
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21")
+    }
+}
+
+val targetJavaVersion = 21
+kotlin {
+    jvmToolchain(targetJavaVersion)
+}
+
+tasks.build {
+    dependsOn("shadowJar")
+}
+
+tasks.processResources {
+    val props = mapOf("version" to version)
+    inputs.properties(props)
+    filteringCharset = "UTF-8"
+    filesMatching("plugin.yml") {
+        expand(props)
+    }
+}
+
+tasks.register<Copy>("copyPlugin") {
+    dependsOn("shadowJar")
+    from(layout.buildDirectory.file("libs/${project.name}-${project.version}.jar"))
+    into("C:/paper1_21_4/plugins")
+}
+
---
+++ src/main/kotlin/sb/ua/rune/RuneEnchantments.kt
@@ -0,0 +1,14 @@
+package sb.ua.rune
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class RuneEnchantments : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
